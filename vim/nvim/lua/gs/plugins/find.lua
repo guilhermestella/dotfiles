7438cf7410d685b4a777d2f19edbc8d07964fbbe
Guilherme Augusto Stella <eng.guilhermestella@gmail.com>
2026-03-02 22:01:26 +0200

    add telescope config to browse edited files

--- vim/nvim/lua/gs/plugins/find.lua
+++ vim/nvim/lua/gs/plugins/find.lua
@@ -16,10 +16,11 @@ return {
                 }
             }
 
-            vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files'})
-            vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
-            vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
-            vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+            vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Search buffers' })
+            vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Help tags' })
+            vim.keymap.set('n', '<leader>ft', builtin.live_grep, { desc = 'Search text' })
+            vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Search files'})
+            vim.keymap.set('n', '<leader>fg', builtin.git_status, { desc = 'Search git status' })
         end
     } 
 }
