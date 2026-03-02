177b3dac0f9db9d655f1afc3a1a1657a873daf7d
Guilherme Augusto Stella <eng.guilhermestella@gmail.com>
2026-03-02 22:46:01 +0200

    add autosave

--- vim/nvim/lua/gs/plugins/code.lua
+++ vim/nvim/lua/gs/plugins/code.lua
@@ -14,5 +14,14 @@ return {
             require('nvim-surround').setup {
             }
         end
-    }
+    },
+    {
+        "pocco81/auto-save.nvim",
+        config = function()
+            require("auto-save").setup({
+                enabled = true,
+                trigger_events = {"InsertLeave", "TextChanged"},
+            })
+        end,
+    } 
 }
