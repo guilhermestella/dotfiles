3c24a4f92452ae217e9f69194c3b4847bb627099
Guilherme Augusto Stella <eng.guilhermestella@gmail.com>
2026-03-03 23:52:49 +0200

    just organize status column

--- vim/nvim/lua/gs/plugins/statuscolumn.lua
+++ vim/nvim/lua/gs/plugins/statuscolumn.lua
@@ -1,26 +1,32 @@
 return {
-    'luukvbaal/statuscol.nvim',
-    config = function()
-        local builtin = require("statuscol.builtin")
-        require('statuscol').setup {
-            relculright = true,
-            segments = {
-                -- Git signs on the left
-                {
-                    sign = { namespace = { "gitsign" }, colwidth = 2, auto = false },
-                    click = "v:lua.ScSa",
+    {
+        'luukvbaal/statuscol.nvim',
+        config = function()
+            local builtin = require("statuscol.builtin")
+
+            require('statuscol').setup {
+                relculright = true,
+                segments = {
+                    { sign = { namespace = { "gitsign" } } },
+                    -- uncomment to add marks (when relevant)
+                    -- {
+                    --     text = {
+                    --         function(args)
+                    --             for _, mark in ipairs(vim.fn.getmarklist(args.buf)) do
+                    --                 if mark.pos[2] == args.lnum then
+                    --                     return mark.mark:sub(-1)
+                    --                 end
+                    --             end
+                    --             return ''
+                    --         end
+                    --
+                    --     }
+                    -- },
+                    { sign = { namespace = { 'diagnostic' } } },
+                    { text = { builtin.foldfunc, " " }, click = "v:lua.ScFa" },
+                    { text = { builtin.lnumfunc, ' ' }, click = 'v:lua.ScLa' },
                 },
-                -- LSP/diagnostic signs on the middle
-                {
-                    sign = { namespace = { ".*" }, name = { ".*" }, colwidth = 2, auto = false },
-                    click = "v:lua.ScSa",
-                },
-                -- Line number in the right
-                {
-                    text = { builtin.lnumfunc, " " },
-                    click = "v:lua.ScLa",
-                },
-            },
-        }
-    end
+            }
+        end
+    }
 }
