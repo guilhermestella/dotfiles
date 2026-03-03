03585626fdfc52ae82c59c14b2ff7250eb7003f1
Guilherme Augusto Stella <eng.guilhermestella@gmail.com>
2026-03-03 11:29:42 +0200

    Add keymaps for telescope and improve git preview

--- vim/nvim/lua/gs/plugins/find.lua
+++ vim/nvim/lua/gs/plugins/find.lua
@@ -12,15 +12,38 @@ return {
                 pickers = {
                     find_files = {
                         theme = 'ivy'
+                    },
+                    git_status = {
+                        previewer = true,
+                        layout_strategy = 'vertical',
+                        layout_config = {
+                            vertical = {
+                                preview_height = 0.6,
+                            }
+                        }
                     }
                 }
             }
 
-            vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Search buffers' })
+
+            -- Find files
+            vim.keymap.set('n', '<C-t>', builtin.find_files, { desc = 'Find files' })
+            vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Find files'})
+
+            -- Search by text
+            vim.keymap.set('n', '<C-f>', builtin.live_grep, { desc = 'Find in files' })
+            vim.keymap.set('n', '<leader>ft', builtin.live_grep, { desc = 'Find in files' })
+
+            -- Find in buffer
+            vim.keymap.set('n', '<M-e>', builtin.buffers, { desc = 'Find in buffers' })
+            vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Find in buffers' })
+
+            -- Find in git status
+            vim.keymap.set('n', '<M-c>', builtin.git_status, { desc = 'Find in git status' })
+            vim.keymap.set('n', '<leader>fg', builtin.git_status, { desc = 'Find in git status' })
+
+            -- Help
             vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Help tags' })
-            vim.keymap.set('n', '<leader>ft', builtin.live_grep, { desc = 'Search text' })
-            vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Search files'})
-            vim.keymap.set('n', '<leader>fg', builtin.git_status, { desc = 'Search git status' })
         end
     } 
 }
