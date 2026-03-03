40fa002abe80eaf5c5a84bfae3beaf7078a1bd2b
Guilherme Augusto Stella <eng.guilhermestella@gmail.com>
2026-03-03 22:25:42 +0200

    modify git keymaps

--- vim/nvim/lua/gs/plugins/git.lua
+++ vim/nvim/lua/gs/plugins/git.lua
@@ -1,6 +1,10 @@
 return {
      {
-         'tpope/vim-fugitive'
+         'tpope/vim-fugitive',
+         cmd = {'G', 'Git'},
+         keys = {
+             { '<C-g>c', ':G<CR>', desc = 'Fugitive Dashboard' }
+         },
      },
      {
          'lewis6991/gitsigns.nvim',
@@ -15,28 +19,28 @@ return {
                          vim.keymap.set(mode, l, r, opts)
                      end
  
-                     map('n', '<leader>gb', gitsigns.blame)
-                     map('n', '<leader>gp', gitsigns.preview_hunk)
-                     map('n', '<leader>gi', gitsigns.preview_hunk_inline)
-                     map('n', '<leader>gr', gitsigns.reset_hunk)
-                     map('n', '<leader>gd', gitsigns.diffthis)
+                     map('n', '<C-g>b', gitsigns.blame)
+                     map('n', '<C-g>d', gitsigns.diffthis)
+                     map('n', '<C-g>p', gitsigns.preview_hunk)
 
-                     map('v', '<leader>gr', function()
+                     -- reset hunk
+                     map('n', '<C-g>r', gitsigns.reset_hunk)
+                     map('v', '<C-g>r', function()
                          gitsigns.reset_hunk({ vim.fn.line('.'), vim.fn.line('v') })
                      end)
  
                      -- Navigation
-                     map('n', '<leader>g]', function()
+                     map('n', '<C-g>]', function()
                          if vim.wo.diff then
-                             vim.cmd.normal({'<leader>gn', bang = true})
+                             vim.cmd.normal({'<C-g>n', bang = true})
                          else
                              gitsigns.nav_hunk('next')
                          end
                      end)
  
-                     map('n', '<leader>g[', function()
+                     map('n', '<C-g>[', function()
                          if vim.wo.diff then
-                             vim.cmd.normal({'<leader>gp', bang = true})
+                             vim.cmd.normal({'<C-g>p', bang = true})
                          else
                              gitsigns.nav_hunk('prev')
                          end
