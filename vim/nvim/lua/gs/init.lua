6e5276a79e5c6e962c6ade47150c1371355845e1
Guilherme Augusto Stella <eng.guilhermestella@gmail.com>
2026-03-04 18:35:27 +0200

    organize plugins by feature

--- vim/nvim/lua/gs/init.lua
+++ vim/nvim/lua/gs/init.lua
@@ -1,3 +1,10 @@
 require('gs.keymaps')
 require('gs.options')
-require('lazy').setup("gs.plugins")
+require('lazy').setup({
+    spec = {
+        { import = 'gs.plugins.ui' },
+        { import = 'gs.plugins.code' },
+        { import = 'gs.plugins.nav' },
+        { import = 'gs.plugins.git' },
+    },
+})
