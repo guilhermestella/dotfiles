cbccd0d9a660df6cf9ac6fa438579688f691c227
Guilherme Augusto Stella <eng.guilhermestella@gmail.com>
2026-03-05 22:09:13 +0200

    add virtual column plugin

---
+++ vim/nvim/lua/core/plugins/ui/virtualcolumn.lua
@@ -0,0 +1,10 @@
+return {
+    {
+        'lukas-reineke/virt-column.nvim',
+        opts = {
+            virtcolumn = '120',
+            char = '┃',
+            display_names = { 'edge' }
+        }
+    }
+}
