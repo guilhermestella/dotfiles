ed8a6cd0dc40995eebeda97005360fcec7461a54
Guilherme Augusto Stella <eng.guilhermestella@gmail.com>
2026-03-06 14:44:48 +0200

    Add back gitsigns
    Add back some gitsigns maps for hunks

--- vim/nvim/lua/core/plugins/git/git.lua
+++ vim/nvim/lua/core/plugins/git/git.lua
@@ -8,5 +8,23 @@ return {
     },
     {
         'lewis6991/gitsigns.nvim',
+        config = function()
+            require('gitsigns').setup {
+                on_attach = function(bufnr)
+                    local gitsigns = require('gitsigns')
+
+                    local function map(mode, l, r, opts)
+                        opts = opts or {}
+                        opts.buffer = bufnr
+                        vim.keymap.set(mode, l, r, opts)
+                    end
+
+                    map('n', '<leader>hr', gitsigns.reset_hunk)
+                    map('v', '<leader>hr', function()
+                        gitsigns.reset_hunk({ vim.fn.line('.'), vim.fn.line('v') })
+                    end)
+                end
+            }
+        end
     }
 }
