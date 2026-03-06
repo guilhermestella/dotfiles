16f5357ed821c80495cf392787ed9f5a4ac86fbf
Guilherme Augusto Stella <eng.guilhermestella@gmail.com>
2026-03-06 19:20:31 +0200

    update some git keymaps

--- vim/nvim/lua/core/plugins/git/git.lua
+++ vim/nvim/lua/core/plugins/git/git.lua
@@ -19,9 +19,27 @@ return {
                         vim.keymap.set(mode, l, r, opts)
                     end
 
+                    map('n', '<leader>hp', gitsigns.preview_hunk)
                     map('n', '<leader>hr', gitsigns.reset_hunk)
                     map('v', '<leader>hr', function()
                         gitsigns.reset_hunk({ vim.fn.line('.'), vim.fn.line('v') })
+                    end)
+
+                    -- Navigation
+                    map('n', ']h', function()
+                        if vim.wo.diff then
+                            vim.cmd.normal({']c', bang = true})
+                        else
+                            gitsigns.nav_hunk('next')
+                        end
+                    end)
+
+                    map('n', '[h', function()
+                        if vim.wo.diff then
+                            vim.cmd.normal({'[c', bang = true})
+                        else
+                            gitsigns.nav_hunk('prev')
+                        end
                     end)
                 end
             }
