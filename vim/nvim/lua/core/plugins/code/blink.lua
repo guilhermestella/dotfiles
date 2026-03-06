46774e627f02e66b8d289bf8a964e8b1af171070
Guilherme Augusto Stella <eng.guilhermestella@gmail.com>
2026-03-06 19:51:41 +0200

    configure lsp with autocomplete

---
+++ vim/nvim/lua/core/plugins/code/blink.lua
@@ -0,0 +1,23 @@
+return {
+    {
+        "saghen/blink.cmp",
+        dependencies = { "rafamadriz/friendly-snippets" },
+        version = "*",
+
+        opts = {
+            keymap = { preset = "default" },
+
+            appearance = {
+                nerd_font_variant = "mono",
+            },
+
+            completion = {
+                documentation = { auto_show = true },
+            },
+
+            sources = {
+                default = { "lsp", "path", "snippets", "buffer" },
+            },
+        }
+    }
+}
