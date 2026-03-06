e6fa362d49f09049d621c91c228038b632f9f9b2
Guilherme Augusto Stella <eng.guilhermestella@gmail.com>
2026-03-06 15:04:39 +0200

    save some keymaps

--- vim/nvim/lua/core/plugins/ui/snacks.lua
+++ vim/nvim/lua/core/plugins/ui/snacks.lua
@@ -54,8 +54,8 @@ return {
         { "<leader>fp", function() Snacks.picker.projects() end, desc = "Projects" },
         { "<leader>fr", function() Snacks.picker.recent() end, desc = "Recent" },
         -- git
-        { "<leader>gb", function() Snacks.picker.git_branches() end, desc = "Git Branches" },
         { "<leader>gg", function() Snacks.lazygit() end, desc = "LazyGit" },
+        { "<leader>gb", function() Snacks.picker.git_branches() end, desc = "Git Branches" },
         { "<leader>gl", function() Snacks.picker.git_log() end, desc = "Git Log" },
         { "<leader>gL", function() Snacks.picker.git_log_line() end, desc = "Git Log Line" },
         { "<leader>gs", function() Snacks.picker.git_status() end, desc = "Git Status" },
@@ -104,5 +104,19 @@ return {
         { "gao", function() Snacks.picker.lsp_outgoing_calls() end, desc = "C[a]lls Outgoing" },
         { "<leader>ss", function() Snacks.picker.lsp_symbols() end, desc = "LSP Symbols" },
         { "<leader>sS", function() Snacks.picker.lsp_workspace_symbols() end, desc = "LSP Workspace Symbols" },
+        -- Other
+        -- { "<leader>z",  function() Snacks.zen() end, desc = "Toggle Zen Mode" },
+        -- { "<leader>Z",  function() Snacks.zen.zoom() end, desc = "Toggle Zoom" },
+        -- { "<leader>.",  function() Snacks.scratch() end, desc = "Toggle Scratch Buffer" },
+        -- { "<leader>S",  function() Snacks.scratch.select() end, desc = "Select Scratch Buffer" },
+        -- { "<leader>n",  function() Snacks.notifier.show_history() end, desc = "Notification History" },
+        { "<leader>bd", function() Snacks.bufdelete() end, desc = "Delete Buffer" },
+        -- { "<leader>cR", function() Snacks.rename.rename_file() end, desc = "Rename File" },
+        { "<leader>gB", function() Snacks.gitbrowse() end, desc = "Git Browse", mode = { "n", "v" } },
+        { "<leader>un", function() Snacks.notifier.hide() end, desc = "Dismiss All Notifications" },
+        -- { "<c-/>",      function() Snacks.terminal() end, desc = "Toggle Terminal" },
+        -- { "<c-_>",      function() Snacks.terminal() end, desc = "which_key_ignore" },
+        -- { "]]",         function() Snacks.words.jump(vim.v.count1) end, desc = "Next Reference", mode = { "n", "t" } },
+        -- { "[[",         function() Snacks.words.jump(-vim.v.count1) end, desc = "Prev Reference", mode = { "n", "t" } },
     }
 }
