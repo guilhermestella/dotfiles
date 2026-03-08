66c086b88a0eeaefb30c5fa7a69a8cf7c4e22861
Guilherme Augusto Stella <eng.guilhermestella@gmail.com>
2026-03-08 21:45:51 +0200

    Configure text objects
    Configure text objects (with treesitter)

--- vim/nvim/lua/core/plugins/code/textobjects.lua
+++ vim/nvim/lua/core/plugins/code/textobjects.lua
@@ -1,6 +1,57 @@
 return {
 	{
 		"nvim-treesitter/nvim-treesitter-textobjects",
-		branch = "main",
+		dependencies = { "nvim-treesitter/nvim-treesitter" },
+		config = function()
+			local select = require("nvim-treesitter-textobjects.select")
+			-- local swap = require("nvim-treesitter-textobjects.swap")
+			local move = require("nvim-treesitter-textobjects.move")
+
+			require("nvim-treesitter-textobjects").setup({
+				select = {
+					lookahead = true,
+					selection_modes = {
+						["@parameter.outer"] = "v",
+						["@function.outer"] = "V",
+						["@class.outer"] = "V",
+					},
+					include_surrounding_whitespace = false,
+				},
+				move = {
+					set_jumps = true,
+				},
+			})
+
+			-- Select
+			vim.keymap.set({ "x", "o" }, "af", function()
+				select.select_textobject("@function.outer", "textobjects")
+			end)
+			vim.keymap.set({ "x", "o" }, "if", function()
+				select.select_textobject("@function.inner", "textobjects")
+			end)
+			vim.keymap.set({ "x", "o" }, "ac", function()
+				select.select_textobject("@class.outer", "textobjects")
+			end)
+			vim.keymap.set({ "x", "o" }, "ic", function()
+				select.select_textobject("@class.inner", "textobjects")
+			end)
+			vim.keymap.set({ "x", "o" }, "aa", function()
+				select.select_textobject("@parameter.outer", "textobjects")
+			end)
+			vim.keymap.set({ "x", "o" }, "ia", function()
+				select.select_textobject("@parameter.inner", "textobjects")
+			end)
+			vim.keymap.set({ "x", "o" }, "as", function()
+				select.select_textobject("@local.scope", "locals")
+			end)
+
+			-- Move
+			vim.keymap.set({ "n", "x", "o" }, "]f", function()
+				move.goto_next_start({ "@function.outer", "@class.outer" }, "textobjects")
+			end)
+			vim.keymap.set({ "n", "x", "o" }, "[f", function()
+				move.goto_previous_start({ "@function.outer", "@class.outer" }, "textobjects")
+			end)
+		end,
 	},
 }
--- vim/nvim/lua/core/plugins/code/treesitter.lua
+++ vim/nvim/lua/core/plugins/code/treesitter.lua
@@ -1,47 +1,40 @@
 return {
-	{
-		"nvim-treesitter/nvim-treesitter",
-		build = ":TSUpdate",
-		lazy = false,
-		config = function()
-			require("nvim-treesitter").setup({
-				ensure_installed = {
-					"bash",
-					"c",
-					"css",
-					"html",
-					"java",
-					"javascript",
-					"json",
-					"latex",
-					"lua",
-					"luadoc",
-					"luap",
-					"markdown",
-					"markdown_inline",
-					"proto",
-					"python",
-					"query",
-					"regex",
-					"rust",
-					"scss",
-					"svelte",
-					"swift",
-					"tsx",
-					"typescript",
-					"vim",
-					"vimdoc",
-					"yaml",
-				},
-				auto_install = true,
-			})
-			-- Enable treesitter-based highlighting and indentation
-			vim.api.nvim_create_autocmd("FileType", {
-				callback = function()
-					pcall(vim.treesitter.start)
-				end,
-			})
-			vim.opt.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
-		end,
-	},
+    {
+        "nvim-treesitter/nvim-treesitter",
+        lazy = false,
+        build = ":TSUpdate",
+        opts = {
+            highlight = { enable = true },
+            auto_install = true,
+            indent = { enable = true },
+            ensure_installed = {
+                "bash",
+                "c",
+                "css",
+                "html",
+                "java",
+                "javascript",
+                "json",
+                "latex",
+                "lua",
+                "luadoc",
+                "luap",
+                "markdown",
+                "markdown_inline",
+                "proto",
+                "python",
+                "query",
+                "regex",
+                "rust",
+                "scss",
+                "svelte",
+                "swift",
+                "tsx",
+                "typescript",
+                "vim",
+                "vimdoc",
+                "yaml",
+            },
+        }
+    },
 }
