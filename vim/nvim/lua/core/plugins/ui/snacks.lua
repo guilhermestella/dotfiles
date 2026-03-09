7243aa4e8b14fce6808f8b6999acc0c9c6ebc785
Guilherme Augusto Stella <eng.guilhermestella@gmail.com>
2026-03-09 11:50:45 +0200

    add better keymap for search selection using picker.grep

--- vim/nvim/lua/core/plugins/ui/snacks.lua
+++ vim/nvim/lua/core/plugins/ui/snacks.lua
@@ -56,7 +56,8 @@ return {
         -- Top Pickers & Explorer
         { "<leader><space>", function() Snacks.picker.smart() end,   desc = "Smart Find Files" },
         { "<leader>,",       function() Snacks.picker.buffers() end, desc = "Buffers" },
-        { "<leader>/",  function() Snacks.picker.grep_word({ live = true }) end, desc = "Grep" },
+        { "<leader>/",  function() Snacks.picker.grep() end, mode = "n", desc = "Grep", },
+        { "/",          function() Snacks.picker.grep_word({ live = true }) end, mode = "x", desc = "Grep selection" },
         { "<leader>:",  function() Snacks.picker.command_history() end,                         desc = "Command History" },
         { "<leader>n",  function() Snacks.picker.notifications() end,                           desc = "Notification History" },
         { "<leader>e",  function() Snacks.explorer() end,                                       desc = "File Explorer" },
