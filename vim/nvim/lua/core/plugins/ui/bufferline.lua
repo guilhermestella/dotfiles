5152499c4d0763c871e4671ad4f5571942c67ec2
Guilherme Augusto Stella <eng.guilhermestella@gmail.com>
2026-03-11 23:26:40 +0200

    comment bufferline

--- vim/nvim/lua/core/plugins/ui/bufferline.lua
+++ vim/nvim/lua/core/plugins/ui/bufferline.lua
@@ -1,26 +1,26 @@
 return {
-	"akinsho/bufferline.nvim",
-	event = "VeryLazy",
-	config = function()
-		require("bufferline").setup({
-			options = {
-				mode = "buffers",
-				numbers = "none",
-				close_command = "bdelete! %d",
-				right_mouse_command = "bdelete! %d",
-				middle_mouse_command = "bdelete! %d",
-				diagnostics = "nvim_lsp",
-				always_show_bufferline = true,
-				separator_style = "thick",
-				indicator = {
-					style = "underline",
-					icon = "▎",
-				},
-				buffer_close_icon = "󰅖",
-				modified_icon = "●",
-				left_trunc_marker = "󰜌",
-				right_trunc_marker = "󰜌",
-			},
-		})
-	end,
+	-- "akinsho/bufferline.nvim",
+	-- event = "VeryLazy",
+	-- config = function()
+	-- 	require("bufferline").setup({
+	-- 		options = {
+	-- 			mode = "buffers",
+	-- 			numbers = "none",
+	-- 			close_command = "bdelete! %d",
+	-- 			right_mouse_command = "bdelete! %d",
+	-- 			middle_mouse_command = "bdelete! %d",
+	-- 			diagnostics = "nvim_lsp",
+	-- 			always_show_bufferline = true,
+	-- 			separator_style = "thick",
+	-- 			indicator = {
+	-- 				style = "underline",
+	-- 				icon = "▎",
+	-- 			},
+	-- 			buffer_close_icon = "󰅖",
+	-- 			modified_icon = "●",
+	-- 			left_trunc_marker = "󰜌",
+	-- 			right_trunc_marker = "󰜌",
+	-- 		},
+	-- 	})
+	-- end,
 }
