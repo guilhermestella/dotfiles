43aeb932a7f153bf7e9f9c9eccc1b5302245b2ca
Guilherme Augusto Stella <eng.guilhermestella@gmail.com>
2026-03-12 23:35:27 +0200

    chage resize keymap

--- .config/nvim/lua/core/config/keymaps.lua
+++ .config/nvim/lua/core/config/keymaps.lua
@@ -33,10 +33,10 @@ vim.keymap.set("n", "]b", "<cmd>bnext<cr>", { desc = "➜ Buffer" })
 -- Tab navigation is defined in tmux.lua plugin
 
 -- Resize
-vim.keymap.set({ "t", "n" }, "<C-Up>", ":resize -2<CR>", { desc = "Increase height" })
-vim.keymap.set({ "t", "n" }, "<C-Down>", ":resize +2<CR>", { desc = "Decrease height" })
-vim.keymap.set({ "t", "n" }, "<C-Left>", ":vertical resize -2<CR>", { desc = "Decrease width" })
-vim.keymap.set({ "t", "n" }, "<C-Right>", ":vertical resize +2<CR>", { desc = "Increase width" })
+vim.keymap.set({ "t", "n" }, "<M-k>", ":resize -2<CR>", { desc = "Increase height" })
+vim.keymap.set({ "t", "n" }, "<M-j>", ":resize +2<CR>", { desc = "Decrease height" })
+vim.keymap.set({ "t", "n" }, "<M-h>", ":vertical resize -2<CR>", { desc = "Decrease width" })
+vim.keymap.set({ "t", "n" }, "<M-l>", ":vertical resize +2<CR>", { desc = "Increase width" })
 
 -- ════════════════════════════════════════════════════════════════════════════
 -- Move Block (Visual Mode)
