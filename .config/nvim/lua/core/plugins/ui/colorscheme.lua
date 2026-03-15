5c0cc151401c7d962a4e6c46949b009348f9acdc
Guilherme Augusto Stella <eng.guilhermestella@gmail.com>
2026-03-15 13:48:10 +0200

    set gruvbox

--- .config/nvim/lua/core/plugins/ui/colorscheme.lua
+++ .config/nvim/lua/core/plugins/ui/colorscheme.lua
@@ -1,19 +1,13 @@
 return {
-	{
-		"catppuccin/nvim",
-		name = "catppuccin",
-		priority = 1000,
-		opts = {
-			flavor = "mocha",
-            transparent_background = true,
-            float = {
-                transparent = true,
-                solid = false,
-            },
-		},
-		config = function(_, opts)
-			require("catppuccin").setup(opts)
-			vim.cmd.colorscheme("catppuccin")
-		end,
-	},
+    {
+        "ellisonleao/gruvbox.nvim",
+        priority = 1000,
+        config = function()
+            require("gruvbox").setup({
+                transparent_mode = true,
+            })
+
+            vim.cmd.colorscheme("gruvbox")
+        end,
+    },
 }
