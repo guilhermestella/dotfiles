c1bec4904ba249424243dca3a9f58b1924fd30b7
Guilherme Augusto Stella <eng.guilhermestella@gmail.com>
2026-03-17 23:00:41 +0200

    improve esc behavior

--- .config/nvim/lua/keymaps/esc.lua
+++ .config/nvim/lua/keymaps/esc.lua
@@ -2,14 +2,11 @@
 -- <Esc> Action
 -- ════════════════════════════════════════════════════════════════════════════
 vim.keymap.set("n", "<Esc>", function()
-  local closed = false
   for _, win in ipairs(vim.api.nvim_list_wins()) do
     if vim.api.nvim_win_get_config(win).relative ~= "" then
       vim.api.nvim_win_close(win, false)
-      closed = true
+      return
     end
   end
-  if not closed then
-    vim.cmd("nohlsearch")
-  end
+  vim.cmd("nohlsearch")
 end, { noremap = true, silent = true, desc = "Esc Action" })
