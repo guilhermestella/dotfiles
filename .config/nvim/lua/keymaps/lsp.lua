8ad0f90676d4f77172f6ff5065d55cdb148d4bc4
Guilherme Augusto Stella <eng.guilhermestella@gmail.com>
2026-03-17 23:12:26 +0200

    fix lsp config warnings

--- .config/nvim/lua/keymaps/lsp.lua
+++ .config/nvim/lua/keymaps/lsp.lua
@@ -1,22 +1,32 @@
 -- ════════════════════════════════════════════════════════════════════════════
 -- LSP
 -- ════════════════════════════════════════════════════════════════════════════
-local hover_opts = { focusable = false, max_height = 25, max_width = 120 }
+local hover_opts = { focusable = true, max_height = 25, max_width = 120 }
 vim.keymap.set("n", "K", function()
   vim.lsp.buf.hover(hover_opts)
 end, { desc = "Hover documentation" })
+
 vim.keymap.set("n", "<F2>", vim.lsp.buf.rename, { desc = "LSP Rename Symbol" })
+
 vim.keymap.set({ "n", "v" }, "<M-CR>", vim.lsp.buf.code_action, { desc = "LSP Code Action" })
 vim.keymap.set({ "n", "v" }, "<leader>la", vim.lsp.buf.code_action, { desc = "➜ Code Action" })
+
 vim.keymap.set({ "n", "v" }, "<leader>lo", function()
   vim.lsp.buf.code_action({
-    context = { only = { "refactor.extract" } },
+    context = {
+      only = { "refactor.extract" },
+      diagnostics = {}
+    },
     apply = true,
   })
 end, { desc = "➜ Extract Method" })
+
 vim.keymap.set("n", "<leader>lo", function()
   vim.lsp.buf.code_action({
-    context = { only = { "source.organizeImports" } },
+    context = {
+      only = { "source.organizeImports" },
+      diagnostics = {}
+    },
     apply = true,
   })
 end, { desc = "➜ Organize Imports" })
