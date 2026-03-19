6a27cfd676114a33930775242742bc0c86e5b2b5
Guilherme Augusto Stella <eng.guilhermestella@gmail.com>
2026-03-19 22:05:50 +0200

    organize options

--- .config/nvim/lua/options/editor.lua
+++ .config/nvim/lua/options/editor.lua
@@ -9,3 +9,17 @@ vim.o.timeoutlen = 500                            -- Time in ms to wait for a ma
 vim.o.confirm = true                              -- Prompt for confirmation instead of failing unsaved changes
 vim.o.autoread = true                             -- Automatically reload files changed outside vim
 vim.o.autowriteall = true                         -- Automatically saves all files when quit
+
+-- ════════════════════════════════════════════════════════════════════════════
+-- Files
+-- ════════════════════════════════════════════════════════════════════════════
+vim.opt.fileencoding = "utf-8"            -- File encoding for new files
+vim.opt.backup = false                    -- Don't create backup files before overwriting
+vim.opt.writebackup = false               -- Don't create backup while editing
+vim.opt.swapfile = false                  -- Don't create swap files
+
+-- ════════════════════════════════════════════════════════════════════════════
+-- Fold
+-- ════════════════════════════════════════════════════════════════════════════
+vim.o.foldlevel = 99
+vim.o.foldlevelstart = 99
--- .config/nvim/lua/options/files.lua
+++
@@ -1,7 +0,0 @@
--- ════════════════════════════════════════════════════════════════════════════
--- Files
--- ════════════════════════════════════════════════════════════════════════════
-vim.opt.fileencoding = "utf-8"            -- File encoding for new files
-vim.opt.backup = false                    -- Don't create backup files before overwriting
-vim.opt.writebackup = false               -- Don't create backup while editing
-vim.opt.swapfile = false                  -- Don't create swap files
