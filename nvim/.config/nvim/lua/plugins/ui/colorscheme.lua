f756d933c5b5c300d7a6aa59e8aecb2ac55993b3
Guilherme Augusto Stella <eng.guilhermestella@gmail.com>
2026-03-25 22:34:47 +0200

    set jellybeans

--- nvim/.config/nvim/lua/plugins/ui/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/ui/colorscheme.lua
@@ -1,13 +1,11 @@
 return {
   {
-    "ellisonleao/gruvbox.nvim",
+    "wtfox/jellybeans.nvim",
+    lazy = false,
     priority = 1000,
+    opts = { },
     config = function()
-      require("gruvbox").setup({
-        transparent_mode = true,
-      })
-
-      vim.cmd.colorscheme("gruvbox")
-    end,
-  },
+      vim.cmd.colorscheme('jellybeans-hc')
+    end
+  }
 }
--- nvim/.config/nvim/lua/plugins/ui/lualine.lua
+++ nvim/.config/nvim/lua/plugins/ui/lualine.lua
@@ -2,22 +2,31 @@ return {
   {
     'nvim-lualine/lualine.nvim',
     dependencies = { 'nvim-tree/nvim-web-devicons' },
-
     config = function()
       require('lualine').setup {
         options = {
           icons_enabled = true,
-          theme = 'gruvbox',
+          theme = 'jellybeans-nvim',
           disabled_filetypes = { "neo-tree" },
         },
         sections = {
           lualine_a = { 'mode' },
-          lualine_b = { 'branch', 'diff', 'diagnostics' },
-          lualine_c = { 'filename' },
+          lualine_b = { 'branch', 'diff', 'diagnostics', 'filename' },
         },
-        tabline = { lualine_a = { 'buffers' } },
-        winbar = { lualine_c = { { 'navic', } } },
-        inactive_winbar = { lualine_c = { { 'navic', } } }
+        inactive_sections = {
+          lualine_a = { 'mode' },
+          lualine_b = { 'branch', 'diff', 'diagnostics', 'filename' },
+          lualine_y = { 'progress' },
+          lualine_z = { 'location' }
+        },
+        winbar = {
+          lualine_a = { 'filename' },
+          lualine_c = { 'navic' },
+        },
+        inactive_winbar = {
+          lualine_a = { 'filename' },
+          lualine_c = { 'navic' },
+        },
       }
     end
   }
