9bc61ef724d03a45eca9c2270bf6ad40f1868d7f
Guilherme Augusto Stella <eng.guilhermestella@gmail.com>
2026-03-26 11:29:55 +0200

    set hl for color column

--- nvim/.config/nvim/lua/plugins/ui/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/ui/colorscheme.lua
@@ -3,9 +3,10 @@ return {
     "wtfox/jellybeans.nvim",
     lazy = false,
     priority = 1000,
-    opts = { },
+    opts = {},
     config = function()
       vim.cmd.colorscheme('jellybeans-hc')
+      vim.api.nvim_set_hl(0, "ColorColumn", { bg = "#303030" })
     end
   }
 }
