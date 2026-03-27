34856daf332a45bd396c39f726b544ba5d29af14
Guilherme Augusto Stella <eng.guilhermestella@gmail.com>
2026-03-27 23:05:11 +0200

    highlight line

--- nvim/.config/nvim/lua/options/ui.lua
+++ nvim/.config/nvim/lua/options/ui.lua
@@ -1,18 +1,23 @@
 -- ════════════════════════════════════════════════════════════════════════════
 -- UI
 -- ════════════════════════════════════════════════════════════════════════════
-vim.o.updatetime = 500
-vim.o.termguicolors = true -- Enable 24-bit RBG colors in terminal
-vim.o.number = true
-vim.o.relativenumber = true
-vim.o.numberwidth = 4 -- Keep a consistent 4-column gutter
-vim.o.wrap = false
-vim.o.breakindent = true -- Wrapped lines preserve indentation
-vim.o.showmode = false -- Don't show mode in command line (shown in statusline)
-vim.o.ruler = false -- Don't show ruler (shown in statusline)
-vim.o.winbar = "%t" -- Filename in winbar (navic will use it if available)
-vim.o.winborder = "rounded" -- Rounded window for neovim
-vim.o.cmdheight = 1 -- Break visual block (https://github.com/neovim/neovim/issues/20635)
-vim.o.colorcolumn = "120" -- Virtual Line at column 120
-vim.o.linebreak = true -- Wrap lines at word boundaries
-vim.opt.shortmess:append "I"
+local set = vim.o
+local seto = vim.opt
+
+set.updatetime = 500
+set.termguicolors = true -- Enable 24-bit RBG colors in terminal
+set.number = true
+set.relativenumber = true
+set.numberwidth = 4 -- Keep a consistent 4-column gutter
+set.wrap = false
+set.breakindent = true -- Wrapped lines preserve indentation
+set.showmode = false -- Don't show mode in command line (shown in statusline)
+set.ruler = false -- Don't show ruler (shown in statusline)
+set.winbar = "%t" -- Filename in winbar (navic will use it if available)
+set.winborder = "rounded" -- Rounded window for neovim
+set.cmdheight = 1 -- Break visual block (https://github.com/neovim/neovim/issues/20635)
+set.colorcolumn = "120" -- Virtual Line at column 120
+set.linebreak = true -- Wrap lines at word boundaries
+set.cursorline = true
+set.cursorlineopt = "screenline"
+seto.shortmess:append "I"
