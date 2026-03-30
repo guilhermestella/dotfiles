b822e1314205f2313152de4bbbc60d158cbe4ef9
Guilherme Augusto Stella <eng.guilhermestella@gmail.com>
2026-03-30 15:31:35 +0300

    reconfigure blink

--- nvim/.config/nvim/lua/plugins/code/blink.lua
+++ nvim/.config/nvim/lua/plugins/code/blink.lua
@@ -2,22 +2,24 @@ return {
   {
     "saghen/blink.cmp",
     dependencies = { "rafamadriz/friendly-snippets" },
-    version = "*",
+    version = "1.*",
 
     opts = {
       keymap = {
-        ["<CR>"] = { "accept", "fallback" },
+        preset = "enter",
         ["<Tab>"] = { "select_next", "fallback" },
-        ["<c-j>"] = { "select_next", "fallback" },
         ["<S-Tab>"] = { "select_prev", "fallback" },
-        ["<c-k>"] = { "select_prev", "fallback" },
       },
+      appearance = {
+        use_nvim_cmp_as_default = true,
+        nerd_font_variant = "mono",
+      },
+      completion = {
+        documentation = { auto_show = true },
+      },
+
       snippets = { preset = "default" },
       signature = { enabled = true },
-      appearance = {
-        use_nvim_cmp_as_default = false,
-        nerd_font_variant = "normal",
-      },
       cmdline = {
         enabled = false,
         completion = { menu = { auto_show = true } },
@@ -25,35 +27,10 @@ return {
           ["<CR>"] = { "accept_and_enter", "fallback" },
         },
       },
-      completion = {
-        menu = {
-          border = "rounded",
-          scrolloff = 1,
-          scrollbar = false,
-          draw = {
-            padding = 1,
-            gap = 1,
-            columns = {
-              { "kind_icon" },
-              { "label", "label_description", gap = 1 },
-              { "kind" },
-              { "source_name" },
-            },
-          },
-        },
-        documentation = {
-          window = {
-            border = "rounded",
-            scrollbar = false,
-            winhighlight = "Normal:BlinkCmpDoc,FloatBorder:BlinkCmpDocBorder,EndOfBuffer:BlinkCmpDoc",
-          },
-          auto_show = true,
-          auto_show_delay_ms = 500,
-        },
-      },
       sources = {
         default = { "lsp", "path", "snippets", "buffer" },
       },
+      fuzzy = { implementation = "prefer_rust" },
     },
   },
 }
