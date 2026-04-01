2222c465abb58b39308332b798c8091597bcc80e
Guilherme Augusto Stella <eng.guilhermestella@gmail.com>
2026-04-01 22:27:08 +0300

    add some navigation remaps improvements

--- nvim/.config/nvim/lua/keymaps/navigation.lua
+++ nvim/.config/nvim/lua/keymaps/navigation.lua
@@ -1,42 +1,22 @@
 -- ════════════════════════════════════════════════════════════════════════════
--- UI Keymaps
+-- Navigation
 -- ════════════════════════════════════════════════════════════════════════════
 local set = vim.keymap.set
 
 -- ════════════════════════════════════════════════════════════════════════════
--- Quit, Save Operation
+-- Window Navigation
 -- ════════════════════════════════════════════════════════════════════════════
-set("n", "<C-w>Q", "<cmd>qa<cr>", { desc = "Quit all" })
-set("n", "<C-w>q", "<cmd>Bdelete!<cr>", { desc = "Delete buffer" })
+-- Navigation and resize are defined in tmux.lua plugin
 
 -- ════════════════════════════════════════════════════════════════════════════
--- Esc Action
+-- Buffer Navigation
 -- ════════════════════════════════════════════════════════════════════════════
-set("n", "<Esc>", function()
-  local win = vim.api.nvim_get_current_win()
-  local config = vim.api.nvim_win_get_config(win)
+-- Bar navigation
+set("n", "<M-j>", "<cmd>bprevious<cr>", { desc = "Previous buffer" })
+set("n", "<M-k>", "<cmd>bnext<cr>", { desc = "Next buffer" })
 
-  -- close only if focused window is a floating window
-  if config.relative ~= "" then
-    vim.api.nvim_win_close(win, false)
-    return
-  end
-
-  -- otherwise, just clear search highlights
-  vim.cmd "nohlsearch"
-end, { noremap = true, silent = true, desc = "Esc Action" })
-
--- ════════════════════════════════════════════════════════════════════════════
--- Marks
--- ════════════════════════════════════════════════════════════════════════════
-set("n", "m", function()
-  local line = vim.fn.line "."
-  local next_char = vim.fn.getcharstr()
-  local mark = vim.fn.getpos("'" .. next_char)
-  if mark[2] == line then
-    vim.cmd("delmarks " .. next_char)
-  else
-    vim.cmd("mark " .. next_char)
-  end
-  vim.cmd "redraw!"
-end)
+-- Content navigation
+set('n', '<C-d>', '<C-d>zz', { desc = 'Scroll downwards' })
+set('n', '<C-u>', '<C-u>zz', { desc = 'Scroll upwards' })
+set('n', 'n', 'nzzzv', { desc = 'Next result' })
+set('n', 'N', 'Nzzzv', { desc = 'Previous result' })
