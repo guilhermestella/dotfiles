cb43f277d5f5cc1366253c9d7d97da6eb2fe1651
Guilherme Augusto Stella <eng.guilhermestella@gmail.com>
2026-04-03 00:35:19 +0300

    toggle blame

--- nvim/.config/nvim/lua/plugins/git/gitsigns.lua
+++ nvim/.config/nvim/lua/plugins/git/gitsigns.lua
@@ -14,6 +14,29 @@ return {
           end, { buffer = bufnr, desc = "➜ Reset" })
           vim.keymap.set("n", "ghR", gitsigns.reset_buffer, { buffer = bufnr, desc = "➜ Reset All" })
 
+          -- Toggle Blame
+          vim.keymap.set("n", "<M-y>", function()
+            local current_buf = vim.api.nvim_get_current_buf()
+
+            if vim.bo[current_buf].filetype == "gitsigns-blame" then
+              vim.cmd "bdelete!"
+              return
+            end
+
+            local found = false
+            for _, win in ipairs(vim.api.nvim_list_wins()) do
+              local buf = vim.api.nvim_win_get_buf(win)
+              if vim.bo[buf].filetype == "gitsigns-blame" then
+                vim.api.nvim_win_close(win, false)
+                found = true
+                break
+              end
+            end
+            if not found then
+              gitsigns.blame { full = false }
+            end
+          end, { desc = "➜ Toggle Blame" })
+
           -- Previous/Next
           vim.keymap.set("n", "[h", function()
             gitsigns.nav_hunk "prev"
