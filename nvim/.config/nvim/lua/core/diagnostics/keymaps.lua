3e27cb8e2c75bebc76de72a63bd9dbb89c391b3c
Guilherme Augusto Stella <eng.guilhermestella@gmail.com>
2026-04-15 21:19:28 +0300

    simplify. show only buffer diagnostics

--- nvim/.config/nvim/lua/core/diagnostics/keymaps.lua
+++ nvim/.config/nvim/lua/core/diagnostics/keymaps.lua
@@ -4,5 +4,4 @@ local fn = require "core.diagnostics.functions"
 set("n", "<C-w>d", fn.open_diagnostic, { desc = "➜ Open Diagnostic" })
 set("n", "[d", fn.goto_previous_diagnostic, { desc = "➜ Previous Diagnostic" })
 set("n", "]d", fn.goto_next_diagnostic, { desc = "➜ Next Diagnostic" })
-set("n", "<M-p>", fn.show_buffer_diagnostics, { desc = "➜ Show Buffer Diagnostics" })
-set("n", "<M-P>", fn.show_diagnostics, { desc = "➜ Show Diagnostics" })
+set("n", "<M-p>", fn.show_buffer_diagnostics, { desc = "➜ Show Diagnostics" })
