69dfc9e7cd124b5d2bfb7e29eeddb4b43452cea4
Guilherme Augusto Stella <eng.guilhermestella@gmail.com>
2026-06-19 23:53:20 +0300

    update to v0.12.3

--- nvim/.config/nvim/lua/core/ui/themes/options.lua
+++ nvim/.config/nvim/lua/core/ui/themes/options.lua
@@ -3,6 +3,12 @@ local fn = require "core.ui.themes.functions"
 
 set.termguicolors = true -- Enable 24-bit RBG colors in terminal
 
+vim.api.nvim_create_autocmd("UIEnter", {
+  callback = function()
+    fn.setup_colorscheme("vscode", "vscode")
+  end,
+})
+
 vim.api.nvim_create_autocmd("OptionSet", {
   pattern = "background",
   callback = function()
